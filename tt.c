3ce2e0637324add119e2b262e1fd7a1c425a4744
aditya69sharma <sharma36aditya@gmail.com>
2022-01-19 15:01:49 +0530

    nothing

---
+++ tt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  int num;
+  printf("Hello Chuttad\n");
+  printf("Koi number daal:");
+  scanf("%d",&num);
+  printf("Ye tera number h %d",num);
+  printf("\nTu to kamaal h bc");
+return 0;
+}
